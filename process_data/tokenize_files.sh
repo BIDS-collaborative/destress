bc6190bb86e488b2c2090f808e8a5ff1add4e9fb
coryschillaci <cdschillaci@gmail.com>
2015-03-12 15:50:36 -0700

    The tokenize_files.sh script now outputs a full list of all the two letter filenames as well as running xmltweet.

--- process_data/tokenize_files.sh
+++ process_data/tokenize_files.sh
@@ -6,11 +6,13 @@
 readonly TOKENIZER=/var/local/destress/scripts/xmltweet.exe  # tokenizer path
 readonly INDIR=/var/local/destress/combined/events
 readonly OUTDIR=/var/local/destress/tokenized
+readonly FILELIST=/var/local/destress/tokenized/fileList.txt
 
 for dat in $(ls ${INDIR}/*.xml); # Loop over all .xml files
 do
     filename=${dat##*/}
     echo $( ${TOKENIZER} -i ${dat} -o ${OUTDIR}/ -d ${OUTDIR}/${filename//.xml/}_dict ) # Runs xmltweet on all the .xml files
+    echo ${filename//.xml/} >> $FILELIST # Write the two letter filename to a list for dictionary merge function in utils.scala
 done
 
 
