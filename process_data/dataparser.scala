6887f7004fb9f863d1339105fbca80570b3fa982
coryschillaci <cdschillaci@gmail.com>
2015-03-12 22:12:33 -0700

    Changed variable name newdict to xmlDict.

--- process_data/dataparser.scala
+++ process_data/dataparser.scala
@@ -6,13 +6,13 @@ import utils._  // utility methods
 
 val dir = "/var/local/destress/tokenized/";
 val xmlFile = loadIMat(dir+"zz.xml.imat");
-val newdict = loadDict(dir+"zz_dict.sbmat",dir+"zz_dict.imat");
+val xmlDict = loadDict(dir+"zz_dict.sbmat",dir+"zz_dict.imat");
 
 
-val usersIdx = getBeginEnd(xmlFile, newdict, "posts");
-val postIdx = getBeginEnd(xmlFile, newdict, "post");
-val eventIdx = getBeginEnd(xmlFile, newdict, "event");
-val moodIdx = getBeginEnd(xmlFile, newdict, "current_moodid");
+val usersIdx = getBeginEnd(xmlFile, xmlDict, "posts");
+val postIdx = getBeginEnd(xmlFile, xmlDict, "post");
+val eventIdx = getBeginEnd(xmlFile, xmlDict, "event");
+val moodIdx = getBeginEnd(xmlFile, xmlDict, "current_moodid");
 
 
 // Get nrUsers
@@ -23,9 +23,9 @@ if (usersIdx(0,0) != -1) {nrUsers = usersIdx.nrows}
 // Get nrValidPosts
 // Posts where <event><string>...</string></event>
 // If </string> is not parsed properly, discard
-val validEvent = find(xmlFile(eventIdx(?,1)-1) == newdict("</string>"));
+val validEvent = find(xmlFile(eventIdx(?,1)-1) == xmlDict("</string>"));
 // If don't want to discard just append
-// find(xmlFile(eventIdx(?,1)-1) == newdict("string"));
+// find(xmlFile(eventIdx(?,1)-1) == xmlDict("string"));
 
 val nrValidPosts = validEvent.nrows;
 // valEventIdx points to <string> +1 and to </string>: do col(0)->col(1)
@@ -34,7 +34,7 @@ val valpostIdx = postIdx(validEvent,?); //assumes postIdx.nrows == eventIdx.nrow
 
 
 // Get nrWords in dictionary
-val nrWords = newdict.cstr.nrows;
+val nrWords = xmlDict.cstr.nrows;
 
 
 // Initialize structures
@@ -57,9 +57,9 @@ while (posti < nrValidPosts) {
       while (postStart > usersIdx(userk, 1)) userk += 1
 
       // Get indices of "current_moodid" open and close tags 
-      val moodIdx = getBeginEnd(xmlFile(postStart -> postEnd), newdict, "current_moodid");
+      val moodIdx = getBeginEnd(xmlFile(postStart -> postEnd), xmlDict, "current_moodid");
       // Check to make sure the tags appear and that it is an int
-      if (moodIdx.nrows==1 && newdict(xmlFile(postStart+moodIdx(0,0))) =="<int>") {
+      if (moodIdx.nrows==1 && xmlDict(xmlFile(postStart+moodIdx(0,0))) =="<int>") {
       	  val moodid = twoComplementToInt(xmlFile(postStart+moodIdx(0,0)+1))(0);
 
 	  // Add to "labels" -> userid, currentmoodId
@@ -83,12 +83,12 @@ while (posti < nrValidPosts) {
 
 //Get not a number values
 //val ind = xmlFile(moodIdx(1,0)+find(xmlFile(moodIdx(1,0)->moodIdx(1,1))>0))
-//newdict(ind)
+//xmlDict(ind)
 //Get number values -> need to be converted
 //val neg = xmlFile(moodIdx(1,0)+find(xmlFile(moodIdx(1,0)->moodIdx(1,1))<0))
 //twoComplementToInt(neg)
 
 
 // moodId -> integer 1->134 (no 94 nor 50)
-//sum(newdict.counts)
-//maxi(newdict.counts)
+//sum(xmlDict.counts)
+//maxi(xmlDict.counts)
