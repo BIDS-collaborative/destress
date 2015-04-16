cfe82c1afa2aa1d6e576ac708c4c29c781dbb5d1
Pierre Karashchuk <krchtchk@gmail.com>
2015-04-15 17:07:40 -0700

    add sentence by sentence featurizer script

---
+++ process_data/featurize_sent.scala
@@ -0,0 +1,3 @@
+import featurizers._
+featurizeMoodIDSentence("/var/local/destress/tokenized2/","/var/local/destress/tokenized2/",
+			"/var/local/destress/featurized_sent/","/var/local/destress/tokenized2/fileList.txt")
