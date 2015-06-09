53c6d28e05f61ca8f0dea2d5e9f2c6f6c644fa2d
Gene Yoo <geneyoo@mercury.dlab.berkeley.edu>
2015-06-08 17:09:55 -0700

    Adding query files for google and LJ word2vec models

--- word2vec/word2vecConvert.py
+++ word2vec/word2vecConvert.py
@@ -1,6 +1,5 @@
 from gensim.models import Word2Vec
 
 if __name__ == '__main__':
-	model = Word2Vec.load('/var/local/destress/word2vecLJ.model')
-	model.save_word2vec_format('/var/local/destress/word2vecLJGoogle1.bin', binary=True)
-
+	model = Word2Vec.load('/var/local/destress/LJ_word2vec/word2vecLJ_2.txt')
+	model.save_word2vec_format('/var/local/destress/LJ_word2vec/word2vecTEXT.txt')
