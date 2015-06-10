88f22fc78c9c6044383b65446c24e1d9d8acfc2e
Gene Yoo <geneyoo@mercury.dlab.berkeley.edu>
2015-06-10 11:17:32 -0700

    trimming trailing newline delimiters in word vectors

--- word2vec/map_matrix.py
+++ word2vec/map_matrix.py
@@ -7,15 +7,9 @@ This is so we can get the correct weights multiplied at each word (or else there
 if __name__ == '__main__':
   masterDict = open('/var/local/destress/LJ_word2vec/list_Dictionary.txt', 'r')
   liveJournal = open('/var/local/destress/LJ_word2vec/word2vecTEXT_copy.txt', 'r')
-  outputMatrix = open('/var/local/destress/LJ_word2vec/matrix_ordered.txt', 'r+')
+  outputMatrix = open('/var/local/destress/LJ_word2vec/matrix_ordered1.txt', 'r+')
 
-<<<<<<< HEAD
-	#have to load in text files as lists
-	masterList = masterDict.readlines()
-	ljList = liveJournal.readlines()
-	
-	ljMap = {}
-=======
+  
   #have to load in text files as lists
   masterList = masterDict.readlines()
   ljList = liveJournal.readlines()
@@ -23,44 +17,29 @@ if __name__ == '__main__':
   liveJournal.close()
   
   ljMap = {}
->>>>>>> 381c1359f74a02e64c2476da9990fff9f18e276b
 
   zeroes = '0.000000 ' * 300
   zeroes = zeroes.strip() 
 
-<<<<<<< HEAD
-	for line in ljList:
-		word, sep, weight = line.partition(" ")
-		ljMap[word] = weight
-	#for k,v in ljMap.items():
-	#	print k, v	
-	
-	count = 1
-	for word in masterList:
-		print count
-		weight = ljMap.get(word)
-		outputMatrix.write(word + ' ')
-		
-		if weight is not None:
-			outputMatrix.write(weight + '\n')
-		else:
-			outputMatrix.write(zeroes + '\n')
-		count += 1
-	masterDict.close()
-	liveJournal.close()
-	outputMatrix.close()
-=======
+  
   for line in ljList:
     word, sep, weight = line.partition(" ")
     ljMap[word] = weight
 
+  #for k,v in ljMap.items():
+  #  outputMatrix.write(k + v + '\n')
+   
   for word in masterList:
-    weight = ljMap.get(word)
+    weight = ljMap.get(word.rstrip())
     outputMatrix.write(word + ' ')
     if weight is not None:
+      print word, 'NOT NONE'
       outputMatrix.write(weight + '\n')
-    else:
+    else: 
+      print word, 'IS NOT IN WORD2VECLIST'
       outputMatrix.write(zeroes + '\n')
-
+  
+  masterDict.close()
+  liveJournal.close()
   outputMatrix.close()
->>>>>>> 381c1359f74a02e64c2476da9990fff9f18e276b
+
