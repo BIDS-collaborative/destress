381c1359f74a02e64c2476da9990fff9f18e276b
Gene Yoo <gyoo@Genes-MacBook-Pro.local>
2015-06-10 09:26:08 -0700

    tabs to spaces

--- word2vec/map_matrix.py
+++ word2vec/map_matrix.py
@@ -5,31 +5,31 @@ This is so we can get the correct weights multiplied at each word (or else there
 
 
 if __name__ == '__main__':
-	masterDict = open('/var/local/destress/LJ_word2vec/list_Dictionary.txt', 'r')
-	liveJournal = open('/var/local/destress/LJ_word2vec/word2vecTEXT_copy.txt', 'r')
-	outputMatrix = open('/var/local/destress/LJ_word2vec/matrix_ordered.txt', 'r+')
+  masterDict = open('/var/local/destress/LJ_word2vec/list_Dictionary.txt', 'r')
+  liveJournal = open('/var/local/destress/LJ_word2vec/word2vecTEXT_copy.txt', 'r')
+  outputMatrix = open('/var/local/destress/LJ_word2vec/matrix_ordered.txt', 'r+')
 
-	#have to load in text files as lists
-	masterList = masterDict.readlines()
-	ljList = liveJournal.readlines()
-	masterDict.close()
-	liveJournal.close()
-	
-	ljMap = {}
+  #have to load in text files as lists
+  masterList = masterDict.readlines()
+  ljList = liveJournal.readlines()
+  masterDict.close()
+  liveJournal.close()
+  
+  ljMap = {}
 
-	zeroes = '0.000000 ' * 300
-	zeroes = zeroes.strip() 
+  zeroes = '0.000000 ' * 300
+  zeroes = zeroes.strip() 
 
-	for line in ljList:
-		word, sep, weight = line.partition(" ")
-		ljMap[word] = weight
+  for line in ljList:
+    word, sep, weight = line.partition(" ")
+    ljMap[word] = weight
 
-	for word in masterList:
-		weight = ljMap.get(word)
-		outputMatrix.write(word + ' ')
-		if weight is not None:
-			outputMatrix.write(weight + '\n')
-		else:
-			outputMatrix.write(zeroes + '\n')
+  for word in masterList:
+    weight = ljMap.get(word)
+    outputMatrix.write(word + ' ')
+    if weight is not None:
+      outputMatrix.write(weight + '\n')
+    else:
+      outputMatrix.write(zeroes + '\n')
 
-	outputMatrix.close()
+  outputMatrix.close()
