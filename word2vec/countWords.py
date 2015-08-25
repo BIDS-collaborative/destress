c984ebcab381219fd2c1244f4cc324b2ae6efb4d
geneyoo <geneyoo@berkeley.edu>
2015-08-25 15:58:48 -0700

    adding script to count words in each sentence

---
+++ word2vec/countWords.py
@@ -0,0 +1,43 @@
+"""  
+Simply counts the words in each sentence (newline) 
+and outputs the counts to a text file
+
+"""
+import logging
+import os.path
+import sys
+import multiprocessing
+
+if __name__ == '__main__':
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+
+    outputCount = open('/var/local/destress/text_sent_ids/word_counts.txt', 'r+')
+    wordCount = {}
+    sentDirectory = '/var/local/destress/text_sent_ids/'
+
+    # fileName = 'sents_1.txt'
+
+    for i in range(1, 1131):
+        print('TEXT FILE NUMBER : ', str(i))
+        fileName = 'sents_' + str(i)+'.txt'
+        textFile = open('/var/local/destress/text_sent_ids/'+fileName)
+        sentences = textFile.readlines() # list of list of words
+        textFile.close()
+
+        for sent in sentences:
+            count = len(sent.split())
+            if count in wordCount:
+                wordCount[count] += 1
+            else:
+                wordCount[count] = 1
+
+    for k, v in wordCount:
+        statStr = str(k) + ' : ' + str(v)   
+        outputCount.write(statStr + '\n')
+    # write the count of the output to the dictionary
+    outputCount.close()
+
