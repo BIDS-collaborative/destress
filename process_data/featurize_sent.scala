613d87c2a4157331a21b5866d9a4aadc4f9bc540
Pierre Karashchuk <krchtchk@gmail.com>
2015-09-11 11:36:17 -0700

    indentation

--- process_data/featurize_sent.scala
+++ process_data/featurize_sent.scala
@@ -1,3 +1,6 @@
 import featurizers._
-featurizeMoodIDSentence("/var/local/destress/tokenized2/","/var/local/destress/tokenized2/",
-			"/var/local/destress/featurized_sent/","/var/local/destress/tokenized2/fileList.txt")
+featurizeMoodIDSentence(
+  "/var/local/destress/tokenized2/",
+  "/var/local/destress/tokenized2/",
+  "/var/local/destress/featurized_sent/",
+  "/var/local/destress/tokenized2/fileList.txt")
